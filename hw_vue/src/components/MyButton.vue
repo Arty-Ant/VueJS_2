d7102cdeebd4ea8984bff5065bd988e884cd2d33
Artem Antonian <art902004@gmail.com>
2025-12-11 21:29:39 +0300

    23

---
+++ hw_vue/src/components/MyButton.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<button @click="">Удалить строку</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ hw_vue/src/components/MyStudentTable.vue
@@ -0,0 +1,48 @@
+<script setup>
+    var students = [
+        {
+        name: "Vasia",
+        age: 19,
+        weight: 67
+        },
+        {
+        name: "Lena",
+        age: 18,
+        weight: 52
+        },
+        {
+        name: "Misha",
+        age: 20,
+        weight: 78
+        },
+        {
+        name: "Katia",
+        age: 17,
+        weight: 45
+        },
+    ]
+</script>
+
+<template>
+    
+    <slot></slot>
+
+    <table border="1">
+            
+            <tbody>
+                <tr v-for = "item in students">
+                    <td>
+                        {{item.name}}</td>
+                    <td>
+                        {{item.age}}</td>
+                    <td>
+                        {{item.weight}}</td>
+                </tr>
+            </tbody>
+            
+    </table>
+</template>
+
+<style scoped>
+
+</style>
